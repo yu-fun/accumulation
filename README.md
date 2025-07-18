bbe368b420aef4720520fdfd6f982502458f9bc6
Xiaoju <xju_zh@163.com>
2025-07-18 17:03:03 +0800

    feat: doc

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # accumulation
+
 knowledge accumulation
+
+vue2 使用 tailwindcss
+
+```bash
+# 安装 tailwindcss 低版本及相关插件
+npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+
+# 创建 postcss.config.js, tailwind.config.js
+npx tailwindcss init -p
+
+# 最后在 main.js 中引入 tailwindcss
+import "tailwindcss/tailwind.css"
+```
+
+```js
+module.exports = {
+  purge: [
+    './src/App.vue',
+    './src/views/**/*.{vue,js,ts,jsx,tsx}',
+    './src/components/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  mode: 'jit', // 是否开启 jit 模式，开启以后编译会更快，当然，tailwindcss 版本需要在 2.1 以上
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
---
+++ tools/crypto.js
@@ -0,0 +1,30 @@
+import fs from "node:fs";
+import crypto from "node:crypto";
+import { pipeline } from "node:stream";
+import { Buffer } from "node:buffer";
+
+const algorithm = "aes-256-cbc";
+const password = "Password used to generate key";
+// const key = Buffer.from(new Uint32Array(Array(32).fill(2)));
+// const iv = Buffer.from(new Uint16Array(Array(16).fill(1)));
+const key = crypto.scryptSync(password, "salt", 32);
+const iv = Buffer.alloc(16, 110);
+
+console.log(key, iv);
+
+// const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+// const input = fs.createReadStream("./chat.zip");
+// const output = fs.createWriteStream("./cc");
+
+// pipeline(input, cipher, output, (err) => {
+//   if (err) throw err;
+// });
+
+// const cipher = crypto.createDecipheriv(algorithm, key, iv);
+// const input = fs.createReadStream("./cc");
+// const output = fs.createWriteStream("./cc.zip");
+
+// pipeline(input, cipher, output, (err) => {
+//   if (err) throw err;
+// });
